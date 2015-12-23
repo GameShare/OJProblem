a3e167182003607bc64d602e9a7b9cb73942d43e
guyao <gamesgao@163.com>
2015-12-23 18:02:06 +0800

    update oj1234 oj1236

--- guyao/oj1234/kruskal.cpp
+++ guyao/oj1234/kruskal.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+
+
 class myNode
 {
     friend class myGraph;
@@ -17,19 +19,184 @@ class myNode
 
 };
 
+class edge
+{
+    friend class myGraph;
+    public:
+        int weight;
+        int head;
+        int rear;
 
+    public:
+        edge()
+        {
+          weight=0;
+          head=0;
+          rear=0;
+        };
+        void setEdge(int h,int r,int w)
+        {
+            weight=w;
+            head=h;
+            rear=r;
+        }
+};
+
+class mySet
+{
+    private:
+        int *data;
+        int maxSize;
+
+    public:
+        mySet(int n)
+        {
+          maxSize=n;
+          data=new int [n];
+          for(int i=0;i<n;i++)
+          {
+              data[i]=-1;
+          }
+        };
+
+        int findRoot(int a)
+        {
+            if(data[a]>=0) return data[a]=findRoot(data[a]);
+            else return a;
+        };
+
+        bool unionSet(int a,int b)
+        {
+            int root1=findRoot(a);
+            int root2=findRoot(b);
+            if(root1!=root2)
+            {
+              if(data[root1]<data[root2])
+              {
+                  data[root1]+=data[root2];
+                  data[root2]=root1;
+              }
+              else
+              {
+                  data[root2]+=data[root1];
+                  data[root1]=root2;
+              }
+              return true;
+            }
+            else return false;
+        };
+
+
+
+        ~mySet()
+        {
+          delete [] data;
+        };
+};
+
+void qsort(edge *eList,int low,int high);
 
 class myGraph
 {
     private:
-        myNode **data;
+        myNode **vertex;
         int vnum;
+        int edgeNum;
         int *marks;
+        edge *allEdge;
 
     public:
+        myGraph(int n,int m)
+        {
+          vertex= new myNode *[n];
+          allEdge=new edge[m];
+          vnum=n;
+          edgeNum=m;
+          marks=new int [n];
+          for(int i=0;i<n;i++)
+          {
+              vertex[i]=NULL;
+              marks[i]=0;
+          }
+        };
+
+        void addEdge(int a,int b,int weight,int m)
+        {
+          allEdge[m].setEdge(a,b,weight);
+          myNode *temp=vertex[a];
+          if(!temp) vertex[a]=new myNode(b);
+          else
+          {
+              while(temp->next) temp=temp->next;
+              temp->next=new myNode(b);
+          }
+
+        };
+
+        int kruskal()
+        {
+            qsort(allEdge,0,edgeNum-1);
+            mySet data(vnum);
+            int ct=vnum;
+            int result=0;
+            for(int i=0;i<edgeNum;i++)
+            {
+                if(data.findRoot(allEdge[i].head)!=data.findRoot(allEdge[i].rear))
+                {
+                    data.unionSet(allEdge[i].head,allEdge[i].rear);
+                    ct--;
+                    result+=allEdge[i].weight;
+                }
+                if(ct==1) break;
+            }
+            return result;
+        };
+
+
+        ~myGraph()
+        {
+          myNode *now;
+          myNode *temp;
+          for(int i=0;i<vnum;i++)
+          {
+              now=vertex[i];
+              while(now)
+              {
+                  temp=now;
+                  now=now->next;
+                  delete temp;
+              }
+          }
+          delete [] vertex;
+          delete [] marks;
+          delete [] allEdge;
+        };
 
 
 };
+
+
+void qsort(edge *eList,int low,int high)
+{
+    if(low>=high) return;
+    int first=low;
+    int last=high;
+    edge mid=eList[first];
+    while(first<last)
+    {
+        while(eList[last].weight>=mid.weight&&first<last) last--;
+        eList[first]=eList[last];
+        while(eList[first].weight<mid.weight&&first<last) first++;
+        eList[last]=eList[first];
+    }
+    eList[first]=mid;
+    qsort(eList,low,first-1);
+    qsort(eList,first+1,high);
+}
+
+
+
+
 
 
 
@@ -38,5 +205,15 @@ class myGraph
 
 int main()
 {
+    int n,m;
+    int a,b,w;
+    cin>>n>>m;
+    myGraph g(n,m);
+    for(int i=0;i<m;i++)
+    {
+        cin>>a>>b>>w;
+        g.addEdge(a-1,b-1,w,i);
+    }
+    cout<<g.kruskal()<<endl;
     return 0;
 }
---
+++ guyao/oj1236/spath.cpp
@@ -0,0 +1,168 @@
+#include<iostream>
+
+using namespace std;
+
+class myNode
+{
+    friend class myGraph;
+    private:
+        int data;
+        int weight;
+        myNode *next;
+
+    public:
+        myNode(int n,int w)
+        {
+          data=n;
+          weight=w;
+          next=NULL;
+        };
+};
+
+class myQueue
+{
+  private:
+    int *data;
+    int maxSize;
+    int head;
+    int rear;
+
+  public:
+    myQueue(int n)
+    {
+      maxSize=n;
+      data=new int [n+1];
+      head=1;
+      rear=0;
+    };
+
+    void enQueue(int n)
+    {
+      if((rear+2)%maxSize==head) return;
+      rear=(rear+1)%maxSize;
+      data[rear]=n;
+    };
+
+    int deQueue()
+    {
+      if((rear+1)%maxSize==head) return -1;
+      int temp=data[head];
+      head=(head+1)%maxSize;
+      return temp;
+    };
+
+    bool isEmpty()
+    {
+      return (rear+1)%maxSize==head;
+    };
+
+    ~myQueue()
+    {
+      delete [] data;
+    };
+};
+
+
+
+class myGraph
+{
+    private:
+        myNode **vertex;
+        int *marks;
+        int vnum;
+
+    public:
+        myGraph(int n)
+        {
+          vnum=n;
+          vertex=new myNode *[n];
+          marks=new int [n];
+          for(int i=0;i<n;i++)
+          {
+              vertex[i]=NULL;
+              marks[i]=0;
+          }
+
+        };
+
+        void addEdge(int a,int b,int w)
+        {
+          myNode *temp=vertex[a];
+          if(!temp) vertex[a]=new myNode(b,w);
+          else
+          {
+            while(temp->next) temp=temp->next;
+            temp->next=new myNode(b,w);
+          }
+        };
+
+        void dijkstra(int start,int ed)
+        {
+          int *distance=new int [vnum];
+          int temp;
+          for(int i=0;i<vnum;i++)
+          {
+            distance[i]=2147483647;
+          }
+          distance[start]=0;
+          myQueue q(vnum+100);
+          q.enQueue(start);
+          myNode *nowPosi;
+          while(!q.isEmpty())
+          {
+            temp=q.deQueue();
+            nowPosi=vertex[temp];
+            while(nowPosi)
+            {
+              if(distance[temp]!=2147483647)
+              {
+                if(distance[nowPosi->data]>distance[temp]+nowPosi->weight)
+                {
+                  q.enQueue(nowPosi->data);
+                  distance[nowPosi->data]=distance[temp]+nowPosi->weight;
+                }
+              }
+              nowPosi=nowPosi->next;
+            }
+          }
+          cout<<distance[ed]<<endl;
+          delete [] distance;
+        };
+
+
+        ~myGraph()
+        {
+          myNode *now;
+          myNode *temp;
+          for(int i=0;i<vnum;i++)
+          {
+            now=vertex[i];
+            while(now)
+            {
+              temp=now;
+              now=now->next;
+              delete temp;
+            }
+          }
+          delete [] vertex;
+          delete [] marks;
+        };
+};
+
+
+
+
+int main()
+{
+  int n,m,start,ed;
+  int a,b,w;
+  cin>>n>>m>>start>>ed;
+  myGraph g(n);
+  for(int i=0;i<m;i++)
+  {
+    cin>>a>>b>>w;
+    g.addEdge(a-1,b-1,w);
+  }
+  g.dijkstra(start-1,ed-1);
+  return 0;
+}
